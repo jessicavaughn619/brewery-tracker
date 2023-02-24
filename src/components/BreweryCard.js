47b4116dbcc015519aa75d44d399b09147644fdb
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-24 13:43:02 -0700

    Remove additional files from template

--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -3,14 +3,14 @@ import React from "react";
 function BreweryCard({ brewery }) {
     const { brewery_type, city, state, name } = brewery;
     return (
-        <div class="brewery-card">
-            <div class="brewery-name">
+        <div className="brewery-card">
+            <div className="brewery-name">
                 {name}
             </div>
-            <div class="brewery-location">
+            <div className="brewery-location">
                 Location: {city}, {state}
             </div>
-            <div class="brewery-type">
+            <div className="brewery-type">
                 Brewery Type: {brewery_type}
             </div>
         </div>
--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -2,7 +2,6 @@ import React from "react";
 import BreweryCard from "./BreweryCard";
 
 function BreweryList({ breweries }) {
-    console.log(breweries)
     const breweryCards = breweries.map((brewery) => (
         <BreweryCard 
             brewery={brewery}
